9fc564fb739917b112dceb64fea0304754fbe3dd
nahnah <nandanrmenon@gmail.com>
2021-01-05 10:32:30 +0530

    Revert "SystemUI: Introduce OOS style notification clear all button"
    This reverts commit 6ae68aa261bf1b73614b37f44f1f770914b8da9b.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -1250,7 +1250,7 @@ public class NotificationPanelViewController extends PanelViewController {
         return !mQsTouchAboveFalsingThreshold;
     }
 
-    public float getQsExpansionFraction() {
+    private float getQsExpansionFraction() {
         return Math.min(
                 1f, (mQsExpansionHeight - mQsMinExpansionHeight) / (mQsMaxExpansionHeight
                         - mQsMinExpansionHeight));
@@ -2156,7 +2156,6 @@ public class NotificationPanelViewController extends PanelViewController {
             alpha *= mClockPositionResult.clockAlpha;
         }
         mNotificationStackScroller.setAlpha(alpha);
-        mStatusBar.updateDismissAllVisibility(true);
     }
 
     private float getFadeoutAlpha() {
